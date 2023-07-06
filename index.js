48c7fddae8cfd5307a5bba2bf8903dfbe9fe72b1
Metarails <SamiSantamaa87@gmail.com>
2023-07-06 12:54:07 +0300

    error update error validation

--- index.js
+++ index.js
@@ -77,18 +77,25 @@ app.post("/api/persons", (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body;
-  
-    const person = {
-        name: body.name,
-        number: body.number,
-    };
-  
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
-      .then(updatedPerson => {
-        response.json(updatedPerson)
-      })
-      .catch(error => next(error));
+    // const body = request.body;
+    const {name , number} = request.body;
+
+    // const person = {
+    //     name: body.name,
+    //     number: body.number,
+    // };
+    
+    console.log("in put parts: ", name, number, request.params.id)
+
+    Person.findByIdAndUpdate(
+        request.params.id,
+            {name, number},
+            { new: true, runValidators: true, context: "query" }
+        )
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error));
   })
 
 app.delete('/api/persons/:id', (request, response, next ) => {
